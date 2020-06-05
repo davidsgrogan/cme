fa0b0ffa7f932be6ec3696709fd923d941758178
David Grogan <dgrogan@chromium.org>
2020-06-04 22:18:07 -0700

    On original full dataset
    Val error bottoms out at 0.2 after 50 epochs

--- model.py
+++ model.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-#import os
-#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+import os
+# This small model is faster on the CPU.
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import tensorflow as tf
+import tensorflow.keras.layers as layers
+from tensorflow.keras.models import Sequential
+import tensorflow.keras.optimizers as optimizers
+from tensorflow.keras.callbacks import TensorBoard
 
 import matplotlib.pyplot as plt
 import IPython.display
 
 import time
 import random
-import os
 import sys
 
-start_time = time.time()
+# https://keras.io/getting_started/faq/#how-can-i-obtain-reproducible-results-using-keras-during-development
+np.random.seed(345)
+tf.random.set_seed(345)
+random.set_seed(345)
 
 # %%
-
 all_data = pd.read_csv('all_data_v7.0.1.csv')
 assert len(all_data.index) == 5295, len(all_data.index)
 
@@ -31,159 +38,92 @@ num_test_samples = 400
 assert ((num_train_samples + num_val_samples + num_test_samples) == len(
     all_data.index)), len(all_data.index)
 
-train_set = all_data.sample()
+train_set, val_test_set = train_test_split(all_data,
+                                           test_size=num_val_samples +
+                                           num_test_samples,
+                                           random_state=35,
+                                           shuffle=True)
 
-X_train, X_val_test = train_test_split(all_data,
-                                       test_size=num_val_samples +
-                                       num_test_samples,
-                                       random_state=35,
-                                       shuffle=True)
+val_set, test_set = train_test_split(val_test_set,
+                                     test_size=num_test_samples,
+                                     random_state=36)
+
+val_test_set = None
+#all_data = None
+
+
+def split_X_y(dataframe):
+  y = dataframe["P_OPS"]
+  X = dataframe.drop(columns="P_OPS")
+  return (X, y)
+
+
+train_X, train_y = split_X_y(train_set)
+val_X, val_y = split_X_y(val_set)
+test_X, test_y = split_X_y(test_set)
+
+train_set = None
+val_set = None
+test_set = None
+
 #%%
 
 # Keras
 model = Sequential()
-model.add(
-    layers.Conv1D(filters=40,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal',
-                  input_shape=(X_train.shape[1], 1)))
+
+model.add(layers.InputLayer(input_shape=train_X.shape[1]))
+
+model.add(layers.Dense(60, activation=None, kernel_initializer='he_normal'))
 model.add(layers.BatchNormalization())
 model.add(layers.LeakyReLU(alpha=0.01))
 
-model.add(
-    layers.Conv1D(filters=50,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal'))
+model.add(layers.Dense(20, activation=None, kernel_initializer='he_normal'))
 model.add(layers.BatchNormalization())
 model.add(layers.LeakyReLU(alpha=0.01))
 
-model.add(
-    layers.Conv1D(filters=60,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal'))
+model.add(layers.Dense(20, activation=None, kernel_initializer='he_normal'))
 model.add(layers.BatchNormalization())
 model.add(layers.LeakyReLU(alpha=0.01))
 
-model.add(keras.layers.MaxPooling1D(pool_size=2))
+model.add(layers.Dense(1, activation=None))
 
-model.add(
-    layers.Conv1D(filters=60,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal'))
-model.add(layers.BatchNormalization())
-model.add(layers.LeakyReLU(alpha=0.01))
+adam_optimizer = optimizers.Adam(lr=0.0001)
+model.compile(optimizer=adam_optimizer, loss='mse', metrics=['mse'])
 
-model.add(
-    layers.Conv1D(filters=60,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal'))
-model.add(layers.BatchNormalization())
-model.add(layers.LeakyReLU(alpha=0.01))
-
-model.add(keras.layers.MaxPooling1D(pool_size=2))
-
-model.add(
-    layers.Conv1D(filters=80,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal'))
-model.add(layers.BatchNormalization())
-model.add(layers.LeakyReLU(alpha=0.01))
-
-model.add(keras.layers.MaxPooling1D(pool_size=2))
-
-model.add(
-    layers.Conv1D(filters=100,
-                  kernel_size=3,
-                  strides=2,
-                  activation=None,
-                  use_bias=False,
-                  kernel_initializer='he_normal'))
-model.add(layers.BatchNormalization())
-model.add(layers.LeakyReLU(alpha=0.01))
-
-model.add(keras.layers.MaxPooling1D(pool_size=2))
-
-model.add(layers.Flatten())
-
-model.add(
-    layers.Dense(40,
-                 activation=None,
-                 use_bias=False,
-                 kernel_initializer='he_normal'))
-model.add(layers.BatchNormalization())
-model.add(layers.LeakyReLU(alpha=0.01))
-
-model.add(layers.Dense(NUM_SPEAKERS, activation='softmax'))
-
-adam_optimizer = keras.optimizers.Adam(lr=0.001)
-model.compile(optimizer=adam_optimizer,
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-keras.utils.plot_model(model,
-                       to_file='test_keras_plot_model.png',
-                       show_shapes=True)
+# keras.utils.plot_model(model,
+#                        to_file='test_keras_plot_model.png',
+#                        show_shapes=True)
 # display(IPython.display.Image('test_keras_plot_model.png'))
 print(model.summary())
 print("Just printed model summary")
 
+#%%
+
 start_time = time.time()
-# with tf.device('/cpu:0'):
-# The baseline model has input size 1911 so we could use batch size 1024.
-# But the CNN has input size 11025, so we have to reduce the batch_size or the
-# GPU runs out of memory.
-tensor_board = keras.callbacks.TensorBoard(histogram_freq=1)
-history_object = model.fit(X_train,
-                           y_train,
-                           epochs=25,
-                           batch_size=64,
-                           verbose=2,
-                           callbacks=[tensor_board],
-                           shuffle=True,
-                           validation_data=(X_dev, y_dev))
+
+tensor_board = TensorBoard(histogram_freq=1)
+
+history_object = model.fit(
+    train_X,
+    train_y,
+    epochs=300,
+    batch_size=64,
+    verbose=2,
+    #callbacks=[tensor_board],
+    shuffle=True,
+    validation_data=(val_X, val_y))
 print("%d seconds to train the model" % (time.time() - start_time))
 
-model.save("cnn_model.h5")
-
-#generate_confusion_matrix(model, test_set_inputs, test_set_labels)
-generate_confusion_matrix(model, X_dev, y_dev)
+#model.save("cnn_model.h5")
 
 # %%
-# Plot training & validation accuracy values
-plt.plot(history_object.history['acc'])
-plt.plot(history_object.history['val_acc'])
-plt.title('CNN Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig('cnn_accuracy.png', bbox_inches='tight')
-plt.close()
-# plt.show()
 
 # Plot training & validation loss values
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('CNN Model Loss')
+omit_first = 10
+plt.plot(history_object.history['mse'][omit_first:])
+plt.plot(history_object.history['val_mse'][omit_first:])
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
+plt.legend(['Train', 'Validation'], loc='upper right')
 plt.savefig('cnn_loss.png', bbox_inches='tight')
-# plt.show()
+plt.show()
